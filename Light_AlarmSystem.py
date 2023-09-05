70f50210a1344a220e9d2303104167781a2cc6e0
CK1 <kreissc1@gmail.com>
2023-09-05 08:50:21 +0200

    Initial Commit

---
+++ Light_AlarmSystem.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+
+###import smbus
+import time
+import datetime as dt
+from pathlib import Path
+
+DEVICE     = 0x23
+POWER_DOWN = 0x00
+POWER_ON   = 0x01
+RESET      = 0x07
+LastAlarmFileName="LastAlarm.txt"
+LightLimit=1000
+AlarmPause= 600 #in Seconds
+#bus = smbus.SMBus(1)
+
+def convertToNumber(data):
+  result=(data[1] + (256 * data[0])) / 1.2
+  return (result)
+
+def readLight(addr=DEVICE):
+  #data = bus.read_i2c_block_data(addr,0x20)
+  #return convertToNumber(data)
+  return 2000
+
+def checkLightForAlarm(lux):
+  if lux >= LightLimit:
+    return True
+  else:
+    return False
+def sendTelegramAlarmPackage():
+  print("Alarm")
+  #todo
+
+def writeLastAlarmTime():
+  currentTime=dt.datetime.now()
+  fileLastAlarm = open(LastAlarmFileName,'w')
+  fileLastAlarm.write(str(currentTime.strftime("%Y-%m-%d %H:%M:%S")))
+  fileLastAlarm.close()
+
+def readLastAlarmTime():
+  fileLastAlarm = open(LastAlarmFileName,'r')
+  strLastAlarm=fileLastAlarm.read()
+  fileLastAlarm.close()
+  return strLastAlarm
+
+def diffLastAlarmToNow():
+  currentTime=dt.datetime.now()
+  strLastAlarm=readLastAlarmTime()
+  LastAlarm= dt.datetime.strptime(strLastAlarm, "%Y-%m-%d %H:%M:%S")
+  delta = currentTime.replace(microsecond=0) - LastAlarm
+  return delta.total_seconds()
+
+def main():
+  #Init
+  path = Path(LastAlarmFileName) #'./LastAlarmFileName'
+  if not path.is_file(): #Create File if it doesn't exist
+    writeLastAlarmTime()
+
+  #Main
+  lightLevel=readLight()
+  print (format(lightLevel,'.2f') + " lux")
+  if checkLightForAlarm(lightLevel):
+    if diffLastAlarmToNow() >= AlarmPause:
+      sendTelegramAlarmPackage()
+      writeLastAlarmTime()
+
+
+if __name__=="__main__":
+   main()
